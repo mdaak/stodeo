6ee2031f2eba7f972b3fe9c4587138d34537dfa6
mdaak <md.aak3@gmail.com>
2022-02-09 19:38:30 +0600

    responsive

--- components/homeComp/FormData.js
+++ components/homeComp/FormData.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import {useform} from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import styles from "../../styles/home/FormData.module.css";
 
 const FormData = () => {
-    return (
-        <div>
-            form deata
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className={styles.form}>
+      <div className={styles.formData}>
+        <div className={styles.formCard}>
+          <div>
+            <h1>Contact Us</h1>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input placeholder="enter your name" {...register("name")} />
+
+            <input
+              placeholder="enter your email"
+              {...register("email", { required: true })}
+            />
+            <br />
+            {errors.email && <spam>email required</spam>}
+
+            <input
+              placeholder="enter your phone"
+              {...register("phone", { required: true })}
+            />
+
+            {errors.phone && <span>phone nunber required</span>}
+
+            <input placeholder="enter your address" {...register("address")} />
+            <br />
+            <textarea
+              id={styles.massage}
+              placeholder="massage"
+              {...register("massage")}
+            />
+            <br />
+            <div id={styles.submitBtn}>
+              <input type="submit" />
+            </div>
+
+            <br />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FormData;
--- components/homeComp/Hero.js
+++ components/homeComp/Hero.js
@@ -1,16 +1,21 @@
-import styles from '../../styles/home/Hero.module.css';
+import styles from "../../styles/home/Hero.module.css";
 
 const Hero = () => {
-    return (
-        <div className={styles.hero}>
-            <div className={styles.heroText}>
-            <h5>WELCOME TO OUR SITE</h5>
-            <h1>We have the best solution for <br/> your  business to grow.</h1>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-            <button>Get started</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroText}>
+        <h5>WELCOME TO OUR SITE</h5>
+        <h1>
+          We have the best solution for <br /> your business to grow.
+        </h1>
+        <p>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts.
+        </p>
+        <button>Get started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
